021879bf0f8a21af24778cb074fbec05e74d6757
Niall Donnelly <niall.donnelly@feedhenry.com>
2017-04-11 12:02:34 +0100

    Converting mongoose documents to JSON for sync data comparison

--- models/workorders.js
+++ models/workorders.js
@@ -1,22 +1,49 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var config = require('./../lib/config');
+var moment = require('moment');
 var labels = config.modelLabels;
 var dataset = config.datasetIDs;
 
+function setTimestamp(time) {
+  return new Date(time).getTime()
+}
+
 var workorderSchema = new Schema({
-  id: {type: String },
-  workflowId: {type: String },
-  assignee: {type: String },
-  type: {type: String },
-  title: {type: String },
-  status: {type: String },
-  startTimestamp: {type: Date },
-  finishTimestamp: {type: Date },
-  address: {type: String },
+  id: {type: String , unique: true, required: true},
+  workflowId: {type: String, required: false },
+  assignee: {type: String, required: false  },
+  type: {type: String, required: true  },
+  title: {type: String, required: true },
+  status: {type: String, required: true, default: "New" },
+  startTimestamp: {type: Number, required: false, set: setTimestamp},
+  finishTimestamp: {type: Number, required: false, set: setTimestamp },
+  address: {type: String, required: true },
   location: [Number],
-  summary: {type: String }
+  summary: {type: String, required: true }
 },{ timestamps: false, strict: false, versionKey: false });
+
+workorderSchema.pre('validate', function(next) {
+  if (this.startTimestamp) {
+
+    if(!moment(this.startTimestamp).isValid()) {
+      return next(new Error("Invalid date value for startTimestamp"));
+    }
+
+    this.startTimestamp = new Date(this.startTimestamp).getTime();
+  }
+
+  if (this.finishTimestamp) {
+
+    if(!moment(this.finishTimestamp).isValid()) {
+      return next(new Error("Invalid date value for finishTimestamp"));
+    }
+
+    this.finishTimestamp = new Date(this.finishTimestamp).getTime();
+  }
+
+  next();
+});
 
 workorderSchema.index({
   id: 1
